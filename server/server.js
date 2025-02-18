a97a22e76e004b69c5ee37e2eea5d86a5f0a1d70
emmeiwhite <emmeiwhite@gmail.com>
2025-02-18 19:42:01 +0530

    timeline-2.1 MongoDB & mongosh installations (brew stuff --- little hectic)

--- server/server.js
+++ server/server.js
@@ -13,6 +13,26 @@ app.use(express.json()) // Parses incoming JSON requests
 app.get('/', (req, res) => {
   // res.send('Chat App Backend is Running...')
   res.json(products)
+
+  // res.send(`<h1>Home Page <a href="/api/products">Products<a/></h1>`)
+})
+
+/* --- 
+{
+"id": 1,
+"name": "Laptop",
+"price": 999.99,
+"description": "A high-performance laptop for work and play.",
+"category": "Electronics",
+"inStock": true
+},
+--- */
+app.get('/api/products', (req, res) => {
+  products.map(product => ({
+    id: product.id,
+    name: product.name,
+    price: product.
+  }))
 })
 
 const PORT = process.env.PORT || 5000
