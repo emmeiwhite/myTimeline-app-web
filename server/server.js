0241c8b1cbe81491ca09047bc0850c370e79fde2
emmeiwhite <emmeiwhite@gmail.com>
2025-02-18 21:23:05 +0530

    timeline-2.7 middleware intercepts the requests before the request reaches the route

--- server/server.js
+++ server/server.js
@@ -2,8 +2,19 @@ const express = require('express')
 const server = express()
 
 // Let's demonstrate middleware
+const logger = (req, res, next) => {
+  const method = req.method
+  const url = req.url
+  const date = new Date().getFullYear()
 
-server.get('/', (req, res) => {
+  console.log(method, url, date) // logging
+  //   res.send('Testing Middleware!')
+  console.log('I am the logger middleware')
+  next()
+}
+
+server.get('/', logger, (req, res) => {
+  console.log('I am the home route')
   res.send('<h1>Home page</h1>')
 })
 
