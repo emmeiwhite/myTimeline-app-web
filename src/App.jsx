e2d03a6c489110fff34fe160ec78ec9134956fb1
emmeiwhite <emmeiwhite@gmail.com>
2025-02-19 07:03:19 +0530

    timeline-3.9 loggedInUser Mocking --- since we are keeping the app simple

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import ChatWindow from './components/ChatWindow'
 import Sidebar from './components/Sidebar'
-import { AppProvider } from './context/MessagesContext'
 
-import streamClient from './streamClient'
+import { useEffect } from 'react'
+import streamClient, { connectUser } from './streamClient'
+import axios from 'axios'
+import { useAppContext } from './context/MessagesContext'
 
 console.log('Stream Client:', streamClient)
 
 function App() {
+  const { loggedInUser } = useAppContext() // Getting our default login user from the context
+
+  useEffect(() => {
+    const fetchTokenAndConnect = async () => {
+      if (!loggedInUser?._id) return // Don't proceed if there's no logged-in user
+      try {
+        const response = await axios.get(`/api/users/token/${loggedInUser}`)
+        const { token } = response.data
+        await connectUser(loggedInUser._id, token)
+      } catch (error) {
+        console.error('Error fetching token:', error)
+      }
+    }
+
+    fetchTokenAndConnect()
+
+    return () => {
+      streamClient.disconnectUser() // Cleanup on unmount
+    }
+  }, [])
+
   return (
-    <AppProvider>
-      <section className="container mx-auto px-4 h-screen flex font-inter">
-        <Sidebar />
-        <ChatWindow />
-      </section>
-    </AppProvider>
+    <section className="container mx-auto px-4 h-screen flex font-inter">
+      <Sidebar />
+      <ChatWindow />
+    </section>
   )
 }
 
--- src/streamClient.js
+++ src/streamClient.js
@@ -1,6 +1,19 @@
 import { StreamChat } from 'stream-chat'
 
-const apiKey = import.meta.env.VITE_STREAM_API_KEY // Store in .env file
+const apiKey = import.meta.env.VITE_STREAM_API_KEY
 const streamClient = StreamChat.getInstance(apiKey)
+
+console.log(`API KEY IS ${apiKey}`)
+export const connectUser = async (userId, token) => {
+  try {
+    await streamClient.connectUser(
+      { id: userId, name: userId }, // You can add more user info here
+      token
+    )
+    console.log(`✅ Connected to Stream as ${userId}`)
+  } catch (error) {
+    console.error('❌ Error connecting to Stream:', error)
+  }
+}
 
 export default streamClient
